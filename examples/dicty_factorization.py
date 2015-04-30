5d2567f1490216c5e1474503bc08dc15bde40648
Marinka Zitnik <marinka@zitnik.si>
2015-04-30 08:31:15 +0200

    Updated dicty example for compatibility with the new API

--- examples/dicty_factorization.py
+++ examples/dicty_factorization.py
@@ -16,8 +16,6 @@ instead of on raw data.
 """
 print(__doc__)
 
-from functools import reduce
-
 from sklearn import cross_validation, ensemble, metrics
 import numpy as np
 
@@ -25,30 +23,26 @@ from skfusion.datasets import load_dicty
 from skfusion import fusion
 
 
-def mf(dicty, train_idx, test_idx, term_idx):
-    max_iter = 10
-    n_run = 1
-    init_type = "random_vcol"
-    random_state = 0
-
-    ann = dicty.ann.data.copy()
+def mf(train_idx, test_idx, term_idx):
+    ann = dicty[gene][go_term][0].data.copy()
     ann[test_idx, :] = 0
-    R_train = {(dicty.gene, dicty.go_term): ann,
-               (dicty.gene, dicty.exprc): dicty.expr.data}
-    T_train = {dicty.gene: [dicty.ppi.data]}
+    relations = [
+        fusion.Relation(ann, gene, go_term),
+        fusion.Relation(dicty[gene][exp_cond][0].data, gene, exp_cond),
+        fusion.Relation(dicty[gene][gene][0].data, gene, gene)]
+    fusion_graph = fusion.FusionGraph(relations)
 
-    fuser = fusion.Dfmf()
+    fuser = fusion.Dfmf(max_iter=10, n_run=1, init_type="random_vcol", random_state=0)
 
     p = 0.7
-    dicty.gene.rank = p * dicty.ann.data.shape[0]
-    dicty.exprc.rank = p * dicty.expr.data.shape[1]
-    dicty.go_term.rank = p * dicty.ann.data.shape[1]
-    fuser.set_scheme(R_train, T_train)
-    fuser.fuse(max_iter, init_type, n_run, random_state=random_state)
-    X = fuser.reconstruct(dicty.gene, dicty.exprc)
+    gene.rank = p * dicty[gene][go_term][0].data.shape[0]
+    exp_cond.rank = p * dicty[gene][exp_cond][0].data.shape[1]
+    go_term.rank = p * dicty[gene][go_term][0].data.shape[1]
+    fuser.fuse(fusion_graph)
+    X = fuser.complete(fusion_graph[gene][exp_cond][0])
 
     X_train = X[train_idx, :]
-    y_train = dicty.ann.data[train_idx, term_idx]
+    y_train = dicty[gene][go_term][0].data[train_idx, term_idx]
     clf = ensemble.RandomForestClassifier(n_estimators=200)
     clf.fit(X_train, y_train)
     X_new = X[test_idx, :]
@@ -56,25 +50,24 @@ def mf(dicty, train_idx, test_idx, term_idx):
     return y_pred
 
 
-def rf(dicty, train_idx, test_idx, term_idx):
-    X_train = dicty.expr.data[train_idx, :]
-    y_train = dicty.ann.data[train_idx, term_idx]
+def rf(train_idx, test_idx, term_idx):
+    X_train = dicty[gene][exp_cond][0].data[train_idx, :]
+    y_train = dicty[gene][go_term][0].data[train_idx, term_idx]
     clf = ensemble.RandomForestClassifier(n_estimators=200)
     clf.fit(X_train, y_train)
-    X_new = dicty.expr.data[test_idx, :]
+    X_new = dicty[gene][exp_cond][0].data[test_idx, :]
     y_pred = clf.predict_proba(X_new)[:, 1]
     return y_pred
 
 
 def main():
     n_folds = 10
-    dicty = load_dicty()
-    n_genes, n_terms = dicty.ann.data.shape
+    n_genes, n_terms = dicty[gene][go_term][0].data.shape
 
     for t, term_idx in enumerate(range(n_terms)):
-        term = dicty.ann.obj2_names[term_idx]
+        term = dicty[gene][go_term][0].col_names[term_idx]
         print("Term: %s" % term)
-        y_true = dicty.ann.data[:, term_idx]
+        y_true = dicty[gene][go_term][0].data[:, term_idx]
 
         cls_size = int(y_true.sum())
         if cls_size > n_genes - 20 or cls_size < 20:
@@ -86,9 +79,9 @@ def main():
         for i, (train_idx, test_idx) in enumerate(skf):
             print("\tFold: %d" % (i+1))
             # Let"s make predictions from fused data representation
-            y_pred_mf[test_idx] = mf(dicty, train_idx, test_idx, term_idx)
+            y_pred_mf[test_idx] = mf(train_idx, test_idx, term_idx)
             # Let"s make predictions from raw data
-            y_pred_rf[test_idx] = rf(dicty, train_idx, test_idx, term_idx)
+            y_pred_rf[test_idx] = rf(train_idx, test_idx, term_idx)
 
         mfa = metrics.roc_auc_score(y_true, y_pred_mf)
         rfa = metrics.roc_auc_score(y_true, y_pred_rf)
@@ -96,4 +89,9 @@ def main():
 
 
 if __name__ == "__main__":
+    dicty = load_dicty()
+    gene = dicty.get_object_type("Gene")
+    go_term = dicty.get_object_type("GO term")
+    exp_cond = dicty.get_object_type("Experimental condition")
+
     main()
