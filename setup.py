ff3b1260e7c72608de468b1c9fddbd42f92ac604
Marinka Zitnik <marinka@zitnik.si>
2015-08-20 12:33:22 +0200

    Added version file

--- setup.py
+++ setup.py
@@ -2,15 +2,19 @@
 descr = """A Python module for data fusion built on top of factorized models."""
 
 from setuptools import setup, find_packages
+import subprocess
+import os
+import imp
 
 DISTNAME = 'scikit-fusion'
 DESCRIPTION = 'A Python module for data fusion built on top of factorized models.'
 LONG_DESCRIPTION = open('README.rst').read()
 MAINTAINER = 'Marinka Zitnik'
 MAINTAINER_EMAIL = 'marinka.zitnik@fri.uni-lj.si'
-URL = 'http://xyz.biolab.si'
+URL = 'http://github.com/marinkaz/scikit-fusion'
 LICENSE = 'GPLv3'
-VERSION = '0.1'
+VERSION = '0.2'
+ISRELEASED = False
 
 INSTALL_REQUIRES = (
     'numpy>=1.7.0',
@@ -18,8 +22,73 @@ INSTALL_REQUIRES = (
     'joblib>=0.8.4',
 )
 
+# Return the git revision as a string
+def git_version():
+    """Return the git revision as a string.
+
+    Copied from numpy setup.py
+    """
+    def _minimal_ext_cmd(cmd):
+        # construct minimal environment
+        env = {}
+        for k in ['SYSTEMROOT', 'PATH']:
+            v = os.environ.get(k)
+            if v is not None:
+                env[k] = v
+        # LANGUAGE is used on win32
+        env['LANGUAGE'] = 'C'
+        env['LANG'] = 'C'
+        env['LC_ALL'] = 'C'
+        out = subprocess.Popen(cmd, stdout = subprocess.PIPE, env=env).communicate()[0]
+        return out
+
+    try:
+        out = _minimal_ext_cmd(['git', 'rev-parse', 'HEAD'])
+        GIT_REVISION = out.strip().decode('ascii')
+    except OSError:
+        GIT_REVISION = "Unknown"
+    return GIT_REVISION
+
+
+def write_version_py(filename='skfusion/version.py'):
+    # Copied from numpy setup.py
+    cnt = """
+# THIS FILE IS GENERATED FROM SCIKIT-FUSION SETUP.PY
+short_version = '%(version)s'
+version = '%(version)s'
+full_version = '%(full_version)s'
+git_revision = '%(git_revision)s'
+release = %(isrelease)s
+
+if not release:
+    version = full_version
+    short_version += ".dev"
+"""
+    FULLVERSION = VERSION
+    if os.path.exists('.git'):
+        GIT_REVISION = git_version()
+    elif os.path.exists('skfusion/version.py'):
+        # must be a source distribution, use existing version file
+        version = imp.load_source("skfusion.version", "skfusion/version.py")
+        GIT_REVISION = version.git_revision
+    else:
+        GIT_REVISION = "Unknown"
+
+    if not ISRELEASED:
+        FULLVERSION += '.dev0+' + GIT_REVISION[:7]
+
+    a = open(filename, 'w')
+    try:
+        a.write(cnt % {'version': VERSION,
+                       'full_version': FULLVERSION,
+                       'git_revision': GIT_REVISION,
+                       'isrelease': str(ISRELEASED)})
+    finally:
+        a.close()
+
 
 def setup_package():
+    write_version_py()
     setup(name=DISTNAME,
           maintainer=MAINTAINER,
           maintainer_email=MAINTAINER_EMAIL,
--- skfusion/__init__.py
+++ skfusion/__init__.py
@@ -1,2 +1,4 @@
 from . import datasets
 from . import fusion
+from .version import \
+    short_version as __version__, git_revision as __git_version__
